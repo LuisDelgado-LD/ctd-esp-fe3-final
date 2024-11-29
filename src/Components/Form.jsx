ca77d406890506a2d204347bac94df63462f3fa9
desktop-local-dev <desktop-local-dev@a.cl>
2024-11-29 10:57:27 -0300

    Formulario completo

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,13 +1,66 @@
-import React from "react";
+import {React, useState} from "react";
 
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
+  const [datosUsuario, setUser] = useState({
+    nombre: "",
+    correo: ""
+  })
+  const [validaciones, setValidaciones] = useState({mostrar:false, error:false});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const correoRegex = /\w+@\w+\.\w{1,3}/;
+    console.log(datosUsuario)
+    if (
+      datosUsuario.nombre.trim().length >= 5 &&
+      datosUsuario.nombre.trim().includes(" ") &&
+      correoRegex.test(datosUsuario.correo)
+    ) {
+      setValidaciones({...validaciones, mostrar:true});
+    } else {
+      setValidaciones({...validaciones, error:true});
+    }
+    // console.log(validaciones)
+  };
+  const handleChange = (event) => {
+    setUser({
+      ...datosUsuario,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const output = () => { 
+    if(validaciones.mostrar){
+      return <div>Gracias {datosUsuario.nombre.split(" ")[0]}, te contactaremos cuando antes vía mail</div>
+    } else if(validaciones.error){
+      return <div>Por favor verifique su información nuevamente</div>
+    } else {
+      return null
+    }
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <label>Nombre:</label>
+        <input
+          type="text"
+          value={datosUsuario.nombre}
+          name="nombre"
+          onChange={handleChange}
+        />
+        <label>Dirección</label>
+        <input
+          type="text"
+          value={datosUsuario.correo}
+          name="correo"
+          onChange={handleChange}
+        />
+      <button>Enviar</button>
       </form>
+      {output()}
     </div>
   );
 };
