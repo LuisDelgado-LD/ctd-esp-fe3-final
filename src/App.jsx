8fac54f0d8f17f491c9c6942b10fce9f8f2f31d9
desktop-local-dev <desktop-local-dev@a.cl>
2024-12-03 21:33:54 -0300

    Se agregan rutas anidadas. Se habilita tema oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,27 @@ import Contact from "./Pages/Contact";
 import Detail from "./Pages/Detail";
 import Favs from "./Pages/Favs";
 import Home from "./Pages/Home";
+import Layout from "./Layout/Layout";
 import { Route, Routes } from "react-router-dom";
+import { useContextGlobal } from "./Components/utils/global.context";
+
 
 function App() {
+  const theme = useContextGlobal().Data.theme;
   return (
-      <div className="App">
-        <Navbar/>
+      <div className={`App ${theme === "dark"? "dark": "light"}`}>
           <Routes>
-            <Route path={paths.home} element={<Home />} />
-            <Route path={paths.Contacto} element={<Contact />} />
-            <Route path={paths.DetallesDinamico} element={<Detail />} />
-            <Route path={paths.Destacados} element={<Favs />} />
-            <Route
-              path={paths.notFound}
-              element={<h1>Page not found - Error 404</h1>}
-            />
+            <Route path={paths.home} element={<Layout />} >
+              <Route path={paths.home} element={<Home />} />
+              <Route path={paths.Contacto} element={<Contact />} />
+              <Route path={paths.DetallesDinamico} element={<Detail />} />
+              <Route path={paths.Destacados} element={<Favs />} />
+              <Route
+                path={paths.notFound}
+                element={<h1>Page not found - Error 404</h1>}
+              />
+            </Route>
           </Routes>
-        <Footer/>
       </div>
   );
 }
