011dbf7ed381440e43ef230f27de5428a50543aa
desktop-local-dev <desktop-local-dev@a.cl>
2024-12-06 04:04:06 -0300

    Cambios esteticos en detalle de dentista Se agregan clases en index: - details - nav .themeButton En Detail.module.css: - se agrega config flex - se agrega config para el boton de favoritos

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useContextGlobal } from "../Components/utils/global.context";
 import Card from '../Components/Card'
 import styles from '../Detail.module.css'
+import FavButt from '../Components/FavButt';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -28,16 +29,22 @@ const Detail = () => {
   const Dentista= Data.dentist
   return (
     <>
-      
+      <div className="details">
       <h1>Detail Dentist id </h1>
       <div className={styles.detailContainer}>
-      <Card dentist={Dentista} className={styles.card}/>
-      <div className={styles.detail}>
-      <h3>Correo de contacto: {Dentista.email}</h3>
-      <h3>Telefono de contacto: {Dentista.phone}</h3>
-      <h3>Sition web: {Dentista.website}</h3>
-      {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
-      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+        <div className={styles.detailRow}>
+          <Card dentist={Dentista} className={styles.card}/>
+            <div className={styles.detail}>
+            <h3>Correo de contacto: {Dentista.email}</h3>
+            <h3>Telefono de contacto: {Dentista.phone}</h3>
+            <h3>Sition web: {Dentista.website}</h3>
+            {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
+            {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+          </div>
+        </div>
+      </div>
+      <div className={styles.favButtonContainer}>
+      <FavButt dentist={Dentista} className={styles.favButton}/>
       </div>
       </div>
     </>
