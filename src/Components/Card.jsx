78b8f555b3f736e14e982aa1e81163c6cf856c68
desktop-local-dev <desktop-local-dev@a.cl>
2024-12-05 21:24:12 -0300

    Se agrega funcionalidad de borrar de favs Se agrega texto en boton fav según si esta en favs o no

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,24 +1,28 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import {React, useState} from "react";
+import { Link } from "react-router-dom";
 import { paths } from "./utils/path";
 
 
-
-
 const Card = ({ dentist }) => {
+  const initialState = (JSON.parse(localStorage.getItem("favList"))||[]).some((fav) => fav.id === dentist.id);
+  const [esFav, setFav] = useState(initialState);
   const name = dentist.name
   const username = dentist.username
   const id = dentist.id
-  // const locate = useLocation();
-  // console.log(locate)
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
-    const favList = JSON.parse(localStorage.getItem("favList")) || [];
-    if (!favList.some((fav) => fav.id === dentist.id)) {
+    const favList = JSON.parse(localStorage.getItem("favList"))||[]
+    if (!esFav) {
       localStorage.setItem("favList", JSON.stringify([...favList,dentist]));
+    } else {
+      const FavsFiltrado = favList.filter(
+        (fav) => fav.id !== dentist.id
+      );
+      localStorage.setItem("favList", JSON.stringify(FavsFiltrado));
+    }
+    setFav(!esFav)
   }
-}
-
+  
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
@@ -30,7 +34,7 @@ const Card = ({ dentist }) => {
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+        <button onClick={addFav} className="favButton">{esFav ?"Del fav":"Add fav"}</button>
     </div>
   );
 };
