90020e1e1555a5eae5c8a82942da6108125e5eae
desktop-local-dev <desktop-local-dev@a.cl>
2024-12-06 03:14:06 -0300

    Función para determinar si mostrar boton o no en card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,5 @@
 import {React, useState} from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { paths } from "./utils/path";
 
 
@@ -9,7 +9,23 @@ const Card = ({ dentist }) => {
   const name = dentist.name
   const username = dentist.username
   const id = dentist.id
+  const locate = useLocation().pathname;
+  // console.log(paths.DetallesEstatico+id)
+  // console.log(locate)
+  const mostrarBoton = () => {
+    console.log("Evaluando boton")
+    if (locate === paths.DetallesEstatico+id){
+       console.log("no mostrar boton")
+      false
+    }
+    else {
+      console.log("mostrar boton")
+      true
+    }
+  }
+  mostrarBoton()
   const addFav = ()=>{
+    // console.log(locate) 
     // Aqui iria la logica para agregar la Card en el localStorage
     const favList = JSON.parse(localStorage.getItem("favList"))||[]
     if (!esFav) {
@@ -34,7 +50,8 @@ const Card = ({ dentist }) => {
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">{esFav ?"Del fav":"Add fav"}</button>
+        {/* <button onClick={addFav} className="favButton">{esFav ?"Del fav":"Add fav"}</button> */}
+        {locate !== paths.DetallesEstatico+id ?  <button onClick={addFav} className="favButton">{esFav ?"Del fav":"Add fav"}</button>: null}
     </div>
   );
 };
